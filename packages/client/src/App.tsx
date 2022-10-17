e707fe58cb154a1ff798cb5868ad49f6f914420e
Mr-LYY <lyulekin.dima@gmail.com>
2022-10-17 21:06:22 +0400

    Feat leaderboard

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -19,14 +19,6 @@ export function App(): JSX.Element {
 
   useEffect(() => {
     checkUserAuth();
-
-    const fetchServerData = async () => {
-      const response = await fetch('http://localhost:3001');
-      const data = await response.json();
-      console.log(data);
-    };
-
-    fetchServerData();
   }, []);
 
   return (
