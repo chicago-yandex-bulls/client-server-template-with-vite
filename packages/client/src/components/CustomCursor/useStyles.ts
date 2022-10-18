12513f9eeb9ecac8dc73b68d3153ef8b3d810391
Mr-LYY <lyulekin.dima@gmail.com>
2022-10-18 13:16:39 +0400

    Fix conflicts

--- packages/client/src/components/CustomCursor/useStyles.ts
+++ packages/client/src/components/CustomCursor/useStyles.ts
@@ -3,8 +3,8 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 export const useStyles = makeStyles(() => ({
   wrapper: {
     position: 'absolute',
-    width: 80,
-    height: 80,
+    width: 50,
+    height: 50,
     pointerEvents: 'none',
     transform: 'translate(-50%, -50%)',
     zIndex: 1000000,
